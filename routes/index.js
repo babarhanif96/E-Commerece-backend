7b7ef64f54fb4b13dc34e2a5cc93e6262eaeee8c
amnariaz007 <amanamana606@gmail.com>
2024-09-30 05:39:08 +0500

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const isLoggedIn = require('../middleware/isLoggedIn');
 const productModel = require('../models/product-model');
 const userModel = require('../models/user-model');
+const mongoose = require('mongoose');
 
 router.get('/', function(req,res) {
     let error = req.flash("error")
@@ -18,7 +19,7 @@ router.get('/shop',isLoggedIn,  async(req,res) =>{
 router.get('/cart',isLoggedIn,  async(req,res) =>{
     let user = await userModel.findOne({email: req.user.email}).populate('cartDetail');
 
-    const bill = (Number(user.cartDetail[0].price)+20)-Number(user.cartDetail[0].discount);
+    const bill = (Number(user.cartDetail.price)+20)-Number(user.cartDetail.discount);
      res.render('cart', {user, bill});
  } )
  
@@ -28,11 +29,33 @@ router.get('/addtocart/:productid',isLoggedIn,  async(req,res) =>{
    let user = await userModel.findOne({email: req.user.email});
    user.cartDetail.push(req.params.productid);
    await user.save();
-
-   
   req.flash("success", "added to the cart");
    res.redirect('/shop');
  } )
+
+
+
+
+router.get('/delete/:productid', isLoggedIn, async (req, res) => {
+    try {
+        // Find the user by their email
+        let user = await userModel.findOne({ email: req.user.email });
+
+        // Convert the req.params.productid to ObjectId
+        const productIdToRemove = new mongoose.Types.ObjectId(req.params.productid);
+
+        // Remove the product from the cartDetail array using $pull
+        user.cartDetail = user.cartDetail.filter(item => item.toString() !== productIdToRemove.toString());
+        await user.save();
+        res.redirect('/cart');
+    } catch (error) {
+        console.error('Error deleting product from cart:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+
+   
 
 router.get('/logout', isLoggedIn,  function(req,res){
     res.render("shop");
